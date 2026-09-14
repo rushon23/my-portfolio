286749485578b176f987c5a3716185550155bd9b
rushon23 <rushon23@users.noreply.github.com>
2026-09-14 08:59:16 +0530

    Remove Vercel preset (Vercel auto-detects Remix Vite)

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 import rehypeImgSize from 'rehype-img-size';
 import rehypeSlug from 'rehype-slug';
 import rehypePrism from '@mapbox/rehype-prism';
-import { vercelPreset } from '@vercel/remix/vite';
 
 export default defineConfig({
   assetsInclude: ['**/*.glb', '**/*.hdr', '**/*.glsl'],
@@ -26,7 +25,6 @@ export default defineConfig({
       providerImportSource: '@mdx-js/react',
     }),
     remix({
-      presets: [vercelPreset()],
       routes(defineRoutes) {
         return defineRoutes(route => {
           route('/', 'routes/home/route.js', { index: true });
