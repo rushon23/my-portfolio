7fc72f77ae862376741a85d7143b7b036ceb3754
Suhas Kembhavi <suhassk2005@gmail.com>
2026-09-14 19:35:07 +0530

    Update role to Developer and update disciplines

--- app/routes/home/home.jsx
+++ app/routes/home/home.jsx
@@ -41,8 +41,8 @@ export const links = () => {
 
 export const meta = () => {
   return baseMeta({
-    title: 'Designer + Developer',
-    description: `Design portfolio of ${config.name} — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
+    title: 'Developer + Designer',
+    description: `Portfolio of ${config.name} — a full-stack developer working on web apps with a focus on high-performance backend systems, AI integration, and beautiful user interfaces.`,
   });
 };
 
