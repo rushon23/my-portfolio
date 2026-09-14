0883d9dde6f72c02caf86810c8c6ccf04ec6829d
rushon23 <rushon23@users.noreply.github.com>
2026-09-14 09:35:38 +0530

    Add new profile picture

--- app/routes/home/profile.jsx
+++ app/routes/home/profile.jsx
@@ -1,11 +1,17 @@
+import profileImgLarge from '~/assets/profile-large.jpg';
+import profileImgPlaceholder from '~/assets/profile-placeholder.jpg';
+import profileImg from '~/assets/profile.jpg';
 import { Button } from '~/components/button';
 import { DecoderText } from '~/components/decoder-text';
 import { Divider } from '~/components/divider';
 import { Heading } from '~/components/heading';
+import { Image } from '~/components/image';
 import { Section } from '~/components/section';
 import { Text } from '~/components/text';
 import { Transition } from '~/components/transition';
 import { Fragment, useState } from 'react';
+import { media } from '~/utils/style';
+import katakana from './katakana.svg';
 import styles from './profile.module.css';
 
 const ProfileText = ({ visible, titleId }) => (
@@ -51,6 +57,34 @@ export const Profile = ({ id, visible, sectionRef }) => {
               >
                 Send me a message
               </Button>
+            </div>
+            <div className={styles.column}>
+              <div className={styles.tag} aria-hidden>
+                <Divider
+                  notchWidth="64px"
+                  notchHeight="8px"
+                  collapsed={!visible}
+                  collapseDelay={1000}
+                />
+                <div className={styles.tagText} data-visible={visible}>
+                  About me
+                </div>
+              </div>
+              <div className={styles.image}>
+                <Image
+                  reveal
+                  delay={100}
+                  placeholder={profileImgPlaceholder}
+                  srcSet={`${profileImg} 480w, ${profileImgLarge} 960w`}
+                  width={960}
+                  height={1280}
+                  sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                  alt="Suhas standing by a lake"
+                />
+                <svg className={styles.svg} data-visible={visible} viewBox="0 0 136 766">
+                  <use href={`${katakana}#katakana-profile`} />
+                </svg>
+              </div>
             </div>
           </div>
         )}
